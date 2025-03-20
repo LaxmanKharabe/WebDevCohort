4745039776b84104c3a00ee3ea3b0d09c300c53b
LaxmanKharabe <laxman31097@gmail.com>
2025-03-20 23:20:26 +0530

    Day 03: changes file name to basicMethod and added Searchingmehods file

---
+++ Array/searchingMethods.js
@@ -0,0 +1,16 @@
+
+// indexOf(): it searches an element in array and returns its index; if the element not found returns -1.
+// array.indexOf(item, start)
+//start: Optional. Where to start the search. Negative values will start at the given position counting from the end, and search to the beginning
+// while searching the string case sensitivity matters.
+// let z = letters.indexOf("Z");
+// console.log(z); // -1
+
+// let b = letters.indexOf("B"); // 1
+// console.log(b);
+
+// let c = letters.indexOf("C", 3);
+// console.log(c); // -1
+// let cindex = letters.indexOf("C", 1);
+// console.log(cindex); // 2
+
